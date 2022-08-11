c5dc20919df5f964603a7e42be63ca929ab34f41
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2022-08-11 17:34:49 +0300

    Made total sum of currencies a live value

--- lib/screens/wallet_page.dart
+++ lib/screens/wallet_page.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:crypto_market/components/animated_card.dart';
 import 'package:crypto_market/db/transaction_database.dart';
 import 'package:crypto_market/model/crypto_transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../model/crypto_model.dart';
 
 extension Iterables<E> on Iterable<E> {
   Map<K, List<E>> groupBy<K>(K Function(E) keyFunction) => fold(<K, List<E>>{},
@@ -19,23 +24,45 @@ class _WalletPageState extends State<WalletPage> {
   double sumOfCurrencies = 0.0;
   List<CryptoTransaction> savedTransactions = [];
   Map<String, List<CryptoTransaction>> groupedTransactions = {};
+  List<CryptoModel> liveCryptoData = [];
+  late bool isDataLoaded;
 
   @override
   void initState() {
     super.initState();
     initSavedTransactions();
+    isDataLoaded = false;
   }
 
   void initSavedTransactions() async {
     savedTransactions = await TransactionDatabase.instance.getTransactions();
-    for (var element in savedTransactions) {
-      sumOfCurrencies +=
-          element.type == TransactionType.buy ? element.amount * element.price : -element.amount * element.price;
-    }
+    liveCryptoData = await fetchCrypto();
 
     setState(() {
+      for (var element in savedTransactions) {
+        liveCryptoData
+            .map((e) => e.currency == element.currency
+                ? sumOfCurrencies += element.type == TransactionType.buy
+                    ? double.parse(e.price) * element.amount
+                    : double.parse(e.price) * element.amount * -1
+                : null)
+            .toList();
+      }
       groupedTransactions = savedTransactions.groupBy((element) => element.currency);
+      isDataLoaded = true;
     });
+  }
+
+  Future<List<CryptoModel>> fetchCrypto() async {
+    final response = await http.get(Uri.parse(
+        'https://api.nomics.com/v1/currencies/ticker?key=b6352825d16d34d26e59f897facc320a11bcd630&interval=1d&status=active&per-page=100&page=1'));
+    final List json = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      return json.map((e) => CryptoModel.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to load crypto model');
+    }
   }
 
   @override
@@ -50,17 +77,19 @@ class _WalletPageState extends State<WalletPage> {
               style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20.0),
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  // My custom card for special use
-                  return AnimatedCard(map: groupedTransactions, index: index);
-                },
-                itemCount: groupedTransactions.length,
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-              ),
-            ),
+            isDataLoaded
+                ? Expanded(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        // My custom card for special use
+                        return AnimatedCard(map: groupedTransactions, index: index);
+                      },
+                      itemCount: groupedTransactions.length,
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                    ),
+                  )
+                : const CircularProgressIndicator(),
           ],
         ),
       ),
