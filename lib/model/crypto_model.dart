668059dbfce1171d64e1c37fb1e467455ecbc90d
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2022-08-11 17:22:26 +0300

    Updated CryptoModel class

--- lib/model/crypto_model.dart
+++ lib/model/crypto_model.dart
@@ -1,18 +1,28 @@
 class CryptoModel {
   final String currency;
   final String price;
+  final String logoUrl;
+  final String priceChangePct;
 
-  const CryptoModel({
-    required this.currency,
-    required this.price,
-  });
+  const CryptoModel({required this.currency, required this.price, required this.logoUrl, required this.priceChangePct});
 
   factory CryptoModel.fromJson(Map<String, dynamic> json) {
-    return CryptoModel(currency: json['currency'], price: json['price']);
+    return CryptoModel(
+        currency: json['currency'],
+        price: json['price'],
+        logoUrl: json['logo_url'],
+        priceChangePct: json['1d']['price_change_pct']);
   }
 
   Map<String, dynamic> toJson() => {
-    'currency': currency,
-    'price': price,
-  };
+        'currency': currency,
+        'price': price,
+        'logo_url': logoUrl,
+        '1d': {'price_change_pct': priceChangePct}
+      };
+
+  @override
+  String toString() {
+    return 'CryptoModel{currency: $currency, price: $price, logoUrl: $logoUrl, priceChangePct: $priceChangePct}';
+  }
 }
