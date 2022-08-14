8d699ed06d5d072ebd20a84822d952cc15e57b3b
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2022-08-14 20:49:36 +0300

    Formatted

--- lib/screens/wallet_page.dart
+++ lib/screens/wallet_page.dart
@@ -9,8 +9,8 @@ import 'package:http/http.dart' as http;
 import '../model/crypto_model.dart';
 
 extension Iterables<E> on Iterable<E> {
-  Map<K, List<E>> groupBy<K>(K Function(E) keyFunction) => fold(<K, List<E>>{},
-      (Map<K, List<E>> map, E element) => map..putIfAbsent(keyFunction(element), () => <E>[]).add(element));
+  Map<K, List<E>> groupBy<K>(K Function(E) keyFunction) =>
+      fold(<K, List<E>>{}, (Map<K, List<E>> map, E element) => map..putIfAbsent(keyFunction(element), () => <E>[]).add(element));
 }
 
 class WalletPage extends StatefulWidget {
